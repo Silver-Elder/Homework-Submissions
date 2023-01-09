1322245a057a1e25188ed0dff5379c74f905bf44
Sterling Jenkins <stelring.silverray@outlook.com>
2023-01-09 09:28:41 -0700

    It turns out that having the deployment target set to 15.0 isn't important for what I thought it would be important for; that is, making the error on the iPhone 13 simulator work. As such, I changed the deployment target back to 16.2. 'Adding a navigation Bar', is now complete.

--- Semester 2 – P1/100 Days SwiftUI–P1-7/P1–WeSplit/WeSplit/ContentView.swift
+++ Semester 2 – P1/100 Days SwiftUI–P1-7/P1–WeSplit/WeSplit/ContentView.swift
@@ -5,55 +5,62 @@
 //  Created by Sterling Jenkins on 1/9/23.
 //
 
-// git commit -m "Changed deployment target /see 'WePlit' next to the blue app icon in left-hand navigation bar/ from iOS 16.2 to 15. This explanded my options for all the different simluators I could run my app on /which is important for the next step of this project: 'Adding a Navigation Bar/.'"
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Form { // This Form has a limit of 10 items that it can hold, but we can overcome that limit by combining different entities into 1 entity, known as a "Group"
-            Group {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
+        NavigationView {
+            Form { // This Form has a limit of 10 items that it can hold, but we can overcome that limit by combining different entities into 1 entity, known as a "Group"
+                /*
+                Group {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                }
+                Group {
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                }
+                Section { // We can also "Group", items using this "Section", command. The difference is that these "Section"s, will visually display the item group seperate form the rest
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                    Image(systemName: "globe")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("Hello, world!")
+                }
+                */
+                Section {
+                    Text("Hello, world!")
+                }
             }
-            Group {
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-            }
-            Section { // We can also "Group", items using this "Section", command. The difference is that these "Section"s, will visually display the item group seperate form the rest
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-                Image(systemName: "globe")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("Hello, world!")
-            }
+            .navigationTitle("SwiftUI") // This is set to display a large title by default. We can change that using this line of code vvv
+            .navigationBarTitleDisplayMode(.inline)
         }
         .padding()
     }
