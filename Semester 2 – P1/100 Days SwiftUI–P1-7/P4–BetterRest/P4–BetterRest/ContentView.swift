c9dc2f503f529852ad43b3066e5f99cccc511f40
Sterling Jenkins <stelring.silverray@outlook.com>
2023-01-18 09:01:10 -0700

    Completed P4 'BetterRest: Wrap up', and created P5 Project 'WordScramble'. P4 complete

--- Semester 2 – P1/100 Days SwiftUI–P1-7/P4–BetterRest/P4–BetterRest/ContentView.swift
+++ Semester 2 – P1/100 Days SwiftUI–P1-7/P4–BetterRest/P4–BetterRest/ContentView.swift
@@ -5,10 +5,21 @@
 //  Created by Sterling Jenkins on 1/17/23.
 //
 
+/* Challenge:
+ "One of the best ways to learn is to write your own code as often as possible, so here are three ways you should try extending this app to make sure you fully understand what’s going on:
+
+ " 1. Replace each VStack in our form with a Section, where the text view is the title of the section. Do you prefer this layout or the VStack layout? It’s your app – you choose!
+ " 2. Replace the “Number of cups” stepper with a Picker showing the same range of values.
+ " 3. Change the user interface so that it always shows their recommended bedtime using a nice and large font. You should be able to remove the “Calculate” button entirely" (Hacking with Swift)
+ */
+
+
+
 import CoreML // This will let up work with our Machine Learning data
     // Also Note: keeping your imports in alphabetical order doesn't matter from a funcitonality standpoint, but it does help you keep your code organized
 import SwiftUI
 
+// Fun Note: "The truly fascinating thing about machine learning is that it doesn’t need big or clever scenarios to be used. You could use machine learning to predict used car prices, to figure out user handwriting, or even detect faces in images. And most importantly of all, the entire process happens on the user’s device, in complete privacy" (Hacking with Swift – BetterRest: Wrap up).
 
 struct ContentView: View {
     
